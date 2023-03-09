b3b4435fdaa2002ccb5c59fc60a0726f8c147e74
noahramdial57 <noahramdial57@gmail.com>
2023-03-09 12:25:58 -0500

    Cleanup

--- preprocess.py
+++
@@ -1,4 +0,0 @@
-meals = ['Breakfast', 'Lunch', 'Dinner']
-allergens = ['Fish', 'Soybeans', 'Wheat', 'Gluten', 'Milk', 'Tree Nuts', 'Eggs', 'Sesame', 'Crustacean Shellfish']
-dietary_restrictions = ['Gluten Friendly', 'Less Sodium', 'Smart Check', 'Vegan', 'Vegetarian', 'Contains Nuts']
-
